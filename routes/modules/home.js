e56be04adf209fe7de493c9d3c18fb5d4d395c95
rubylo718 <rubylo718@gmail.com>
2022-04-15 17:59:15 +0800

    feat: add sorting function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,23 @@ import Restaurants from '../../models/restaurants.js'
 router.get('/', (req, res) => {
   Restaurants.find()
     .lean()
-    // .sort({ _id: 'asc'})
+    .sort({_id: 'asc'})
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+// sorting by selected method
+router.get('/sort', (req, res) => {
+  let sortMethod = req.query.sortMethods
+  switch (sortMethod) { 
+    case "1": sortMethod = {name: 'asc'}; break;
+    case "2": sortMethod = {name: 'desc'}; break;
+    case "3": sortMethod = {category:'asc'}; break;
+    case "4": sortMethod = {location: 'asc'}; break;
+  }  
+  Restaurants.find()
+    .lean()
+    .sort(sortMethod)
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
