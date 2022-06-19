e7efb6bbe25a4188d0c40860ef7d9473839b4901
rubylo718 <rubylo718@gmail.com>
2022-06-19 23:12:03 +0800

    feat: rewrite the seeder

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -2,7 +2,6 @@ function authCheck (req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   }
-  console.log('authcheck failed')
   req.flash('warning_msg', 'Please login in.')
   res.redirect('/users/login')
 }
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,16 +1,41 @@
 import 'dotenv/config'
+import bcrypt from 'bcryptjs'
 import Restaurants from '../restaurants.js'
+import User from '../users.js'
 import db from '../../config/mongoose.js'
 
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url) // construct the require method
-const restaurantList = require('../../restaurant.json') // use the require method
+const restaurantList = require('../../restaurant.json').results // use the require method
+
+const SEED_USERS = [
+  { email: 'user1@example.com', password: '12345678', ownedRestarantId: [1, 2, 3] },
+  { email: 'user2@example.com', password: '12345678', ownedRestarantId: [4, 5, 6] }
+]
 
 db.once('open', () => {
   console.log('mongodb connected! run the seeder')
-  Restaurants.create(restaurantList.results)
+  Promise.all(
+    SEED_USERS.map(async function (seedUser) {
+      const hash = await bcrypt.hash(seedUser.password, 10)
+      const newUser = new User({ email: seedUser.email, password: hash })
+      try {
+        await newUser.save()
+        const userId = newUser._id
+        return Promise
+          .all(restaurantList.map(restaurant => {
+            if (seedUser.ownedRestarantId.includes(restaurant.id)) {
+              restaurant.userId = userId
+              return Restaurants.create(restaurant)
+            }
+          }))
+      } catch (err) {
+        console.log('sth went wrong', err)
+      }
+    })
+  )
     .then(() => {
-      console.log('done')
+      console.log('seeder done')
       process.exit()
     })
 })
